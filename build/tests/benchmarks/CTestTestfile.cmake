ba7275fb2f84af8a74d6430d0a8de6d841d2d47f
awesome <3109372170@qq.com>
2025-02-07 13:16:10 +0800

    Remove build directory and add to .gitignore

--- build/tests/benchmarks/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/awe/Desktop/clion/large_project_template/tests/benchmarks
-# Build directory: /home/awe/Desktop/clion/large_project_template/build/tests/benchmarks
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tests/unit_tests[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[MyLibTest.AddFunction]=]  /home/awe/Desktop/clion/large_project_template/build/tests/unit_tests [==[--gtest_filter=MyLibTest.AddFunction]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[MyLibTest.AddFunction]=]  PROPERTIES WORKING_DIRECTORY /home/awe/Desktop/clion/large_project_template/build/tests SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  unit_tests_TESTS MyLibTest.AddFunction)
